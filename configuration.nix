970a7d7527dd3fe18afd7ad87f638c8645210314
Jeri <jer@Jeris-MacBook-Air.local>
2023-07-09 14:44:51 -0500

    added docker + jer

--- configuration.nix
+++ configuration.nix
@@ -6,12 +6,24 @@
   networking.useDHCP = true;
   networking.networkmanager.enable = false;
 
+  
   users.users = {
     root = {
-      initialHashedPassword = "rootHash_placeholder";
-      openssh.authorizedKeys.keys = [ "sshKey_placeholder" ];
+      initialHashedPassword = "$6$jhFedIJGrSRMLQfV$u.oyQzAiWpRhkcwJRWYoX3HgEzxHXrY/VuAOaCikHPCzht7.euRtSTzJ/4EdWFBXxpBNfMrT2mhI3J1tuEYpI0";
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFnJ1RYcyw/WBT7CSfvG5fRL591X90Gy1S87BGZPTUmt root@ssd" ];
+      extraGroups = [ "docker" ];
+    };
+    jer = {
+      isNormalUser = true;
+      home = "/home/jer";
+      description = "Jeri V. Luckenbaugh";
+      extraGroups = [ "wheel" "docker" ];
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPN0rcHxQVA3/ItRzxhd5eUmIzlUPihAPEP4zkQ/n7Oi jluckenbaugh@ucsd.edu" ];
+      initialHashedPassword = "$6$b9oWhqTmy5w/DNyQ$NjzLIEB9n/i0aGzo5nKlP0kgxUORUgkY4bxdfsfAevFtBsMNhKJI3HIMmitq9tQfdrGTVdRa7PrP7CCSoUjh6/";
     };
   };
+
+
 
   programs.neovim = {
     enable = true;
@@ -41,4 +53,7 @@
       jq # other programs
     ;
   };
+
+  # docker setup
+  virtualisation.docker.enable = true;
 }
