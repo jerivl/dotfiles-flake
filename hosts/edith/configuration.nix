40c84af4e836d8b0bc94a5639a70b84f80a66982
Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>
2023-07-10 01:46:29 +0200

    added exa to jer
    Signed-off-by: Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>

--- hosts/edith/configuration.nix
+++ hosts/edith/configuration.nix
@@ -13,5 +13,10 @@
     extraGroups = [ "wheel" "docker" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPN0rcHxQVA3/ItRzxhd5eUmIzlUPihAPEP4zkQ/n7Oi jluckenbaugh@ucsd.edu" ];
     initialHashedPassword = "$6$b9oWhqTmy5w/DNyQ$NjzLIEB9n/i0aGzo5nKlP0kgxUORUgkY4bxdfsfAevFtBsMNhKJI3HIMmitq9tQfdrGTVdRa7PrP7CCSoUjh6/";
+    packages = builtins.attrValues {
+      inherit (pkgs)
+        exa
+      ;
+    };
   };
 }
