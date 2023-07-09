bcd6b1ff4cd3f9a324c3bfc5f54e114281575958
Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>
2023-07-10 01:26:42 +0200

    refactored jer to my-config
    Signed-off-by: Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>

--- configuration.nix
+++ configuration.nix
@@ -13,14 +13,6 @@
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPN0rcHxQVA3/ItRzxhd5eUmIzlUPihAPEP4zkQ/n7Oi jluckenbaugh@ucsd.edu" ];
       extraGroups = [ "docker" ];
     };
-    jer = {
-      isNormalUser = true;
-      home = "/home/jer";
-      description = "Jeri V. Luckenbaugh";
-      extraGroups = [ "wheel" "docker" ];
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPN0rcHxQVA3/ItRzxhd5eUmIzlUPihAPEP4zkQ/n7Oi jluckenbaugh@ucsd.edu" ];
-      initialHashedPassword = "$6$b9oWhqTmy5w/DNyQ$NjzLIEB9n/i0aGzo5nKlP0kgxUORUgkY4bxdfsfAevFtBsMNhKJI3HIMmitq9tQfdrGTVdRa7PrP7CCSoUjh6/";
-    };
   };
 
 
--- modules/my-config/default.nix
+++ modules/my-config/default.nix
@@ -9,6 +9,14 @@ in {
     };
   };
 
+  users.users.jer = {
+    isNormalUser = true;
+    home = "/home/jer";
+    description = "Jeri V. Luckenbaugh";
+    extraGroups = [ "wheel" "docker" ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPN0rcHxQVA3/ItRzxhd5eUmIzlUPihAPEP4zkQ/n7Oi jluckenbaugh@ucsd.edu" ];
+    initialHashedPassword = "$6$b9oWhqTmy5w/DNyQ$NjzLIEB9n/i0aGzo5nKlP0kgxUORUgkY4bxdfsfAevFtBsMNhKJI3HIMmitq9tQfdrGTVdRa7PrP7CCSoUjh6/";
+  };
   # if my-config.template.desktop.gnome.enable is set to true
   # set the following options
   config = mkIf config.my-config.template.desktop.gnome.enable {
