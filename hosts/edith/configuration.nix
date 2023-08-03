bbd7eeff1b444ce56011dd50b8e4390a032d97f3
Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>
2023-08-03 23:43:01 +0200

    add ssh update edith

--- hosts/edith/configuration.nix
+++ hosts/edith/configuration.nix
@@ -35,7 +35,7 @@
     script = ''
       cd /home/jer/edith
       /run/current-system/sw/bin/git config --global --add safe.directory /home/jer/edith
-      /run/current-system/sw/bin/ssh-agent /bin/sh -c '/run/current-system/sw/bin/ssh-add /home/jer/.ssh/id_ed25519; git pull'
+      /run/current-system/sw/bin/ssh-agent /bin/sh -c '/run/current-system/sw/bin/ssh-add /home/jer/.ssh/id_ed25519; /run/current-system/sw/bin/git pull'
       /run/current-system/sw/bin/docker compose down
       /run/current-system/sw/bin/docker compose up
     '';
