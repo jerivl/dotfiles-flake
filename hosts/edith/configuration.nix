17eee7ec97874264fb9940dfe986a8d61a9829de
Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>
2023-08-04 00:57:43 +0200

    add ssh update edith

--- hosts/edith/configuration.nix
+++ hosts/edith/configuration.nix
@@ -35,7 +35,7 @@
       Type = "oneshot";
       User = "jer";
       WorkingDirectory= "/home/jer/edith";
-      ExecStart = "git pull; docker compose down; docker compose up";
+      ExecStart = "/run/current-system/sw/bin/echo $PATH; git pull; docker compose down; docker compose up";
     };
     path = [ pkgs.git pkgs.openssh pkgs.docker ];
   };
