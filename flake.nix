842d407bbdc008595f6624604029b19518a2f23d
Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>
2023-08-12 23:25:15 +0200

    added unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
