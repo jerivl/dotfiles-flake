e391b3bab1446f4a51dc4c4f068676f8230f2e58
Jeri V. Luckenbaugh <jluckenbaugh@ucsd.edu>
2023-08-23 23:10:52 +0200

    refactoring edith

--- hosts/edith/configuration.nix
+++ hosts/edith/configuration.nix
@@ -1,30 +1,5 @@
 { pkgs, ... }: {
   # configuration in this file only applies to edith host.
-  programs.tmux = {
-    enable = true;
-    newSession = true;
-    terminal = "tmux-direct";
-  };
-  users.users.jer = {
-    isNormalUser = true;
-    home = "/home/jer";
-    description = "Jeri V. Luckenbaugh";
-    extraGroups = [ "wheel" "docker" ];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWKM8yGHV5cBlQ5SSyFjCZBgzc/ebtKfmyHQ7mWvxXZ jluckenbaugh@ucsd.edu" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmYjfbjlSZkpImpnfN0eftss2bptDoZ6TCmMLghAbhS+8HIJeYEXH0nYSZ2PO0WR+t7gQajNqR2i0FnGy1sEgIog6cB/ddJFybWU2oPmYT7iK3G46q0/BHhZL7Z9LVIIVjInbmZh64okLmvooccf+lbHUdgdobdNTRyh4e+rzxKxwnonQXYwjc5uNlpp7YD/4dkI8/7q0gW7X6in2HqeqUxfY6BvOygAT8qNOKNvhBN58RZYW1YRMlUy89xD2h819CqPg5qvTGNrVM51eLWN+CWCCzb3v9mpXOn+MR7n92WbR7D+3sm5d+2iK7MGfb6wpJAV4l/SofbIF9XCC8WcC3KyrWkqwlYq8fC3v3cbT7NTQ+msknCgzmrMpEtkyQMn4Sj/yuUB7zKuGHTb1C0rFPQ9HSc8/E4cKjaXEYNRiTIiz4YARV+2jbCLjC044gLZPApmZX3wwJyYwwu1jVyQR//T2TKGfCPf7iLoDChXLJaP2I8hYB6n+WKMftHPY3ZeM="];
-    initialHashedPassword = "$6$b9oWhqTmy5w/DNyQ$NjzLIEB9n/i0aGzo5nKlP0kgxUORUgkY4bxdfsfAevFtBsMNhKJI3HIMmitq9tQfdrGTVdRa7PrP7CCSoUjh6/";
-    packages = builtins.attrValues {
-      inherit (pkgs)
-        exa
-	sd
-	du-dust
-	fd
-	certbot
-	lazygit
-	lazydocker
-	ctop
-      ;
-    };
-  };
   systemd.timers."update-edith" = {
     wantedBy = [ "timers.target" ];
     timerConfig = {
@@ -42,16 +17,6 @@
     };
     path = [ pkgs.git pkgs.openssh pkgs.docker ];
     script = "git pull; docker stop $(docker ps -a -q); docker compose up -d";
-  };
-
-  # Root email alias
-  environment.etc = {
-    "aliases" = {
-      text = ''
-        root: admin@jerivl.com
-      '';
-      mode = "0644";
-    };
   };
 
   programs.msmtp = {
